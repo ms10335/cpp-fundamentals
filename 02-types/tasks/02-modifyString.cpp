c2d8c7086b71c0104861cf1057b79d9bb507bd3d
Smoktunowicz Marcin <msmoktunowicz@upc.pl>
2021-05-11 21:25:27 +0200

    02-myfing.cpp

--- 02-types/tasks/02-modifyString.cpp
+++ 02-types/tasks/02-modifyString.cpp
@@ -4,6 +4,14 @@
 // TODO: Implement modifyString()
 // It should modify passed string to text "Other string"
 
+std::string modifyString(std::string& text)
+{
+    return text="Other string";
+}
+
+
+
+
 int main() {
     std::string str{"Some string"};
     modifyString(str);
