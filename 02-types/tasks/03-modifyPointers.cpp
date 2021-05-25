9f8fa5b4ca0b19d628fdca7a3d68f69fa80d0230
Marcin$ <marcin.s10335@gmail.com>
2021-05-25 21:56:17 +0200

    Complete foo() bar () as asked in question

--- 02-types/tasks/03-modifyPointers.cpp
+++ 02-types/tasks/03-modifyPointers.cpp
@@ -5,6 +5,9 @@
 // bar() should modify value under passed pointer to 20
 // Can we have a pointer to const or a const pointer?
 
+void bar(int *const ptr);
+void foo(int * const ptr);
+
 int main() {
     int number = 5;
     int* pointer = &number;
@@ -15,4 +18,14 @@ int main() {
     std::cout << number << '\n';
 
     return 0;
+}
+
+//we can use simple pointer OR const ptr to modyfie values
+void bar(int * const ptr)
+{
+    *ptr = 20;
+}
+void foo(int * const ptr)
+{
+    *ptr = 10;
 }
