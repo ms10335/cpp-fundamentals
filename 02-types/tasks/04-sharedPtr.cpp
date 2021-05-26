87fa80151430c3d5e27ca44d4410a6e63b0676d3
Marcin$ <marcin.s10335@gmail.com>
2021-05-26 21:25:05 +0200

    Fill 04-SharepPtr.cpp with foo() function

--- 02-types/tasks/04-sharedPtr.cpp
+++ 02-types/tasks/04-sharedPtr.cpp
@@ -6,11 +6,17 @@
 // It should also display the value of this int and the number of how many pointers are pointing to it - use `use_count()`.
 // Display the same information in main() before and after calling foo()
 
+void foo(std::shared_ptr<int> num) {
+    *num = 20;
+    std::cout << "IN foo() function:" <<num << " " << *num << " " << " number of shared_pointer  owners: " << num.use_count() << '\n';
+}
+
 int main() {
     std::shared_ptr<int> number = std::make_shared<int>(10);
     // display the value under number pointer and use_count() of it
+    std::cout << " IN main() function " <<number << " " << *number << " " << " number of shared_pointer owners  : " << number.use_count() << '\n';
     foo(number);
     // display the value under number pointer and use_count() of it
-
+   std::cout << " IN main() function " << number << " " << *number << " " << " number of shared_pointer owners: " << number.use_count() << '\n';
     return 0;
 }
